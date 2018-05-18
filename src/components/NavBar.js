58230112d7f8f08ecc1ec731489fb8f0bbaf92cc
sbein <sbein@umich.edu>
2018-05-18 09:56:56 -0400

    Initial Commit.

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+  render(){
+    return(
+      <div>
+        <ul className = "NavBar">
+            <li className = "nav-item nav-name"> SBEIN </li>
+            <li className = "nav-item nav-last"> <a href = "#footer" className="nav-link">Contact</a></li>
+            <li className = "nav-item"> <a href = "#Resume" className="nav-link">Contact</a></li>
+            <li className = "nav-item"> <a href = "#Blog" className="nav-link">Blog</a></li>
+            <li className = "nav-item"> <a href = "#Projects" className="nav-link">Projects</a></li>
+            <li className = "nav-item"> <a href = "#About" className="nav-link">About</a></li>
+          </ul>
+        </div>
+    );
+  }
+}
+
+export default NavBar;
